8648efb08bd04adf26594787b9708d44dce78a40
S-o-y-k-a <discordsoyka@gmail.com>
2024-10-01 20:56:10 +0400

    -

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: read-file
+
+read-file: read-file.cpp
+	g++ read-file.cpp -o read-file
+
+clear:
+	rm read-file
---
+++ read-file.cpp
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <iostream>
+#include <fcntl.h>
+
+int main (int argc, char *argv[]) {
+	char buffer[50];
+	if(argc != 2) {
+		return 1;
+	}
+	int fd = open(argv[1], O_RDONLY);
+	if(fd == -1) {
+		perror("Opening file error");
+		exit(EXIT_FAILURE);
+	}
+	ssize_t r_bytes;
+	while (r_bytes != -1) {
+	       	r_bytes = read(fd, buffer, sizeof(buffer) - 1);	
+	buffer[r_bytes] = '\0';
+	std::cout << buffer;
+	}
+
+	close(fd);
+
+	return 0;
+}
